bdfea1f4121f2905398ffc0cf7814f7877838ec7
salehaparveen <salehaparveen1994@gmail.com>
2023-06-09 12:23:51 -0700

    pushed a svelte form

---
+++ README.md
@@ -0,0 +1,1 @@
+## See it running: https://public.yuritoledo1.now.sh
---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Form from "./Pages/Form.svelte";
+  import Counter from "./Pages/Counter.svelte";
+  import { counter } from "./Store/store";
+
+  let counterValue;
+  counter.subscribe(val => (counterValue = val));
+</script>
+
+<Form />
+<Counter />
+<h1>Contador: {counterValue}</h1>
